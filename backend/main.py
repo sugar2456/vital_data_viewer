966b817996510b1b48775719fcabff296f0d48cf
Ogawa Satoru <sugar2456_0526@yahoo.co.jp>
2024-04-27 18:09:09 +0900

    nextjsの環境整備

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,17 @@
-from fastapi import FastAPI,Request
+import strawberry
+
+from fastapi import FastAPI
+from strawberry.fastapi import GraphQLRouter
+
+@strawberry.type
+class Query:
+    @strawberry.field
+    def hello(self) -> str:
+        return "Hello World"
+
+schema = strawberry.Schema(Query)
+
+graphql_app = GraphQLRouter(schema)
 
 app = FastAPI()
-
-
-@app.get("/api")
-async def root(request:Request):
-    print(request.headers)
-    return {"message": "Hello World"}
-
-@app.get("/no-proxy-header")
-async def noProxyHeader(request:Request):
-    print(request.headers)
-    return {"message": "no proxy header"}
+app.include_router(graphql_app, prefix="/graphql")
