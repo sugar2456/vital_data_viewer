9b4cb64f7295c415fa9e4771042966ce57f25ffa
sugar2456 <sugar2456_0526@yahoo.co.jp>
2025-01-09 21:47:27 +0900

    歩数を取得するAPIを実装

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI, Depends
 from src.api.endpoints import fitbit_auth
+from src.api.endpoints import fitbit_activity
 
 app = FastAPI()
 
 app.include_router(fitbit_auth.router, prefix="/api", tags=["fitbit_auth"])
+app.include_router(fitbit_activity.router, prefix="/api", tags=["fitbit_activity"])
 
 if __name__ == "__main__":
     import uvicorn
