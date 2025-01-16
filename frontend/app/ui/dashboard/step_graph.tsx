cb1702f3b11be40237e7c29340b5da5e4abea65c
sugar2456 <sugar2456_0526@yahoo.co.jp>
2025-01-16 15:22:56 +0900

    ダッシュボード画面：歩数のカードを作成

--- frontend/app/ui/dashboard/step_graph.tsx
+++ frontend/app/ui/dashboard/step_graph.tsx
@@ -4,63 +4,59 @@ import React from 'react';
 import { Line } from 'react-chartjs-2';
 import { Chart, TimeScale, LinearScale, PointElement, LineElement, Tooltip, Legend } from 'chart.js';
 import 'chartjs-adapter-date-fns';
-import { toZonedTime } from 'date-fns-tz';
 import stepsData from '@/app/ui/data/step/steps_intraday.json';
 import { ChartOptions } from 'chart.js';
 
 Chart.register(TimeScale, LinearScale, PointElement, LineElement, Tooltip, Legend);
 
 const StepsChart: React.FC = () => {
-    const timeZone = 'Asia/Tokyo';
-
     // データの整形
     const times = stepsData.steps_intraday.map((entry: { time: string }) => {
-      const utcDate = new Date(`1970-01-01T${entry.time}Z`);
-      return toZonedTime(utcDate, timeZone);
+        return new Date(`1970-01-01T${entry.time}+09:00`);
     });
+    console.log(times);
     const values = stepsData.steps_intraday.map((entry: { value: number }) => entry.value);
-  
+
     const data = {
-      labels: times,
-      datasets: [
-        {
-          label: 'Steps',
-          data: values,
-          fill: false,
-          backgroundColor: 'rgba(75,192,192,0.4)',
-          borderColor: 'rgba(75,192,192,1)',
-        },
-      ],
-    };
-  
-    const options: ChartOptions<'line'> = {
-      scales: {
-        x: {
-          type: 'time',
-          time: {
-            unit: 'hour',
-            tooltipFormat: 'HH:mm',
-            displayFormats: {
-              hour: 'HH:mm',
+        labels: times,
+        datasets: [
+            {
+                label: 'Steps',
+                data: values,
+                fill: false,
+                backgroundColor: 'rgba(75,192,192,0.4)',
+                borderColor: 'rgba(75,192,192,1)',
             },
-          },
-        //   adapters: {
-        //     date: {
-        //       locale: 'ja-JP',
-        //     },
-        //   },
+        ],
+    };
+
+    const options: ChartOptions<'line'> = {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+            x: {
+                type: 'time',
+                time: {
+                    unit: 'hour',
+                    tooltipFormat: 'HH:mm',
+                    displayFormats: {
+                        hour: 'HH:mm',
+                    },
+                },
+            },
+            y: {
+                beginAtZero: true,
+            },
         },
-        y: {
-          beginAtZero: true,
-        },
-      },
     };
 
 
     return (
-        <div className="p-4 bg-white border border-gray-200 rounded-lg shadow-md">
-            <h2 className="text-lg font-semibold mb-4">Steps Intraday</h2>
-            <Line data={data} options={options} />
+        <div className="rounded-xl bg-gray-50 shadow-sm p-2">
+            <h3 className="ml-4 text-sm font-medium m-4">1日の歩数</h3>
+            <div className="h-auto bg-white">
+                <Line data={data} options={options} />
+            </div>
         </div>
     );
 };
