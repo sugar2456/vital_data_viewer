28701b56a880c5501be9f5a63de1f61423c312c0
sugar2456 <sugar2456_0526@yahoo.co.jp>
2025-01-17 15:57:47 +0900

    ダッシュボード睡眠カード実装

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,9 +1,10 @@
 import { Card } from '../ui/dashboard/cards';
+import { SleepCard } from '../ui/dashboard/sleep_card';
 import StepsChart from '../ui/dashboard/step_graph';
 
 export default function Page() {
     const cardData: { title: string; value: number; type: 'sleep' | 'step' | 'activity' | 'weight' }[] = [
-        { title: 'Sleep', value: 8, type: 'sleep' },
+        // { title: 'Sleep', value: 8, type: 'sleep' },
         { title: 'Steps', value: 10000, type: 'step' },
         { title: 'Activity', value: 30, type: 'activity' },
         { title: 'Weight', value: 70, type: 'weight' },
@@ -11,6 +12,7 @@ export default function Page() {
 
     return (
         <div className="p-4 grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+            <SleepCard />
             {cardData.map((card, index) => (
                 <Card key={index} title={card.title} value={card.value} type={card.type} />
             ))}
